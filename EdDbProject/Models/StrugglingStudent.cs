9ffc7d1c1ecc975abe2423b5f99de40582ca8a5d
Jeffrey North <JNorth@tql.com>
2021-06-01 10:36:58 -0400

    Add project files.

---
+++ EdDbProject/Models/StrugglingStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EdDbProject.Models
+{
+    public partial class StrugglingStudent
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal YearBorn { get; set; }
+    }
+}
---
+++ EdDbProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using EdDbProject.Models;
+using System;
+using System.Linq;
+
+
+namespace EdDbProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EdDbContext();
+            /* var studentsInKY = context.Students
+              /                .Where(s => s.StateCode == "KY")
+                                 OrderBy(s => s.Lastname)
+                                 .ToList();
+             foreach(var s in studentsInKY)
+             {
+                 Console.WriteLine($"{s.Firstname} {s.Lastname}");  */
+            //Ky students by lastname query syntax ..join has to be in order in sql
+            //below brings back ky students and joined with major
+            var studentsWithMajors = from s in context.Students
+                                     join m in context.Majors
+                                     on s.MajorId equals m.Id
+                                     where s.StateCode == "KY"
+                                     orderby s.Lastname
+                                     select new {
+                                         s.Firstname,s.Lastname, Major = m.Description};
+            foreach(var s in studentsWithMajors)
+            {
+                Console.WriteLine($"{s.Firstname} { s.Lastname} || { s.Major}");
+            }
+
+        }
+
+            
+
+        
+    }
+}
